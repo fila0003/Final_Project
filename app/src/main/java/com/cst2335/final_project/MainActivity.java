d20f799b2a8390f913fe9dee14fc5f1e8a2a74b4
Evgenii Filatov <fila0003@algonquinlive.com>
2021-03-21 14:37:10 -0400

    Initial commit

---
+++ app/src/main/java/com/cst2335/final_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cst2335.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
